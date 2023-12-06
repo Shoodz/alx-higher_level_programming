4713d6607420b6977d71991cdefbe140715c49c0
shahd <shoodyomer22214@gmail.com>
2023-12-06 14:11:15 -0800

    m

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    num = 0
+    list_keys = list(a_dictionary.keys())
+
+    for i in list_keys:
+        num += 1
+
+    return (num)
